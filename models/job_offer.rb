5077cc983549a173ff9c879001d4667490dd2840
celedituro <cdituro@fi.uba.ar>
2022-10-08 18:09:00 -0300

    feat: adding salary to job_offer

--- models/job_offer.rb
+++ models/job_offer.rb
@@ -3,9 +3,10 @@ class JobOffer
 
   attr_accessor :id, :user, :user_id, :title,
                 :location, :description, :is_active,
-                :updated_on, :created_on
+                :updated_on, :created_on, :salary
 
   validates :title, presence: true
+  validates :salary, numericality: { only_integer: true, allow_nil: true, greater_than_or_equal_to: 0 }
 
   def initialize(data = {})
     @id = data[:id]
@@ -16,6 +17,7 @@ class JobOffer
     @updated_on = data[:updated_on]
     @created_on = data[:created_on]
     @user_id = data[:user_id]
+    @salary = data[:salary].blank? ? nil : data[:salary]
     validate!
   end
 
@@ -37,5 +39,11 @@ class JobOffer
 
   def old_offer?
     (Date.today - updated_on) >= 30
+  end
+
+  def salary_valid?
+    return false if salary.nil?
+
+    salary.positive?
   end
 end
