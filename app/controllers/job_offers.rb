e13860d8a28c0cf006e5d47363137f56c6bc0243
alejovillores <avillores@fi.uba.ar>
2022-10-17 15:39:41 -0300

    fix: fix editing job offer date field

--- app/controllers/job_offers.rb
+++ app/controllers/job_offers.rb
@@ -23,6 +23,7 @@ JobVacancy::App.controllers :job_offers do
   get :edit, with: :offer_id do
     @job_offer = JobOfferForm.from(JobOfferRepository.new.find(params[:offer_id]))
     @edit = true
+    @value = @job_offer.expired_date
     # TODO: validate the current user is the owner of the offer
     render 'job_offers/edit'
   end
